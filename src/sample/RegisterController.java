c3bf86d0fca6a6856724fa259bf68f2912d257d6
Kacper Kromka <kacper.kromkaa@gmail.com>
2020-03-16 18:42:27 +0100

    second panel

---
+++ src/sample/RegisterController.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class RegisterController {
+
+    @FXML
+    private TextField loginTxt;
+
+    @FXML
+    private PasswordField passwordTxt;
+
+    @FXML
+    private PasswordField passwordTxt2;
+
+    @FXML
+    private Label errorLbl;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    public ButtonBar register;
+
+}
